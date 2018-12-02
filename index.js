8a4a79d179251bdeb62be624c5744bb56053b85f
Carrie Forde <cforde@bills.com>
2018-12-02 15:15:41 -0800

    Fixes issue with DOM library

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const Validator = require('javascript/validator');
-const DOMUtilities = require('javascript/dom-utilities');
 
-module.exports = { Validator, DOMUtilities };
+module.exports = Validator;
